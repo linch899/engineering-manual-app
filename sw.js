2a1957bdfd73f03b6220260cda4ec47ebbd21771
linch899 <linch899@gmail.com>
2026-05-26 15:17:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-// 最簡 Service Worker，僅為滿足 PWA 安裝觸發條件
-self.addEventListener('fetch', function(event) {
-  // 不做任何攔截，直接放行網路請求
+// 強制立即接管控制權
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// 必須要有 fetch 事件監聽，這是 Chrome 允許「安裝」的硬性規定
+self.addEventListener('fetch', event => {
+  // 不做任何快取邏輯，直接放行網路請求
 });
